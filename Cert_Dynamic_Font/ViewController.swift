82fef881ba45bd67c4bc1e0c69b6d8f8b1ebaaaa
Himchan <himchan05@naver.com>
2021-03-17 14:28:29 +0900

    Initial Commit

---
+++ Cert_Dynamic_Font/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cert_Dynamic_Font
+//
+//  Created by 박힘찬 on 2021/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
